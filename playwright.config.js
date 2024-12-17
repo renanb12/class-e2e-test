b2d7d85aa15fdd90a71927901ac81b1a61e1d64e
05632593029 <05632593029@senacrs.edu.br>
2024-12-16 21:35:32 -0300

    Finalizado

--- playwright.config.js
+++ playwright.config.js
@@ -30,9 +30,9 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
-    screenshot: 'on'
-/*    
+    screenshot: 'on',
     headless: false,
+/*    
 
     launchOptions: {
       slowMo: 500
